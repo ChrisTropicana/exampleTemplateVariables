d51d77edfcd0e7496e3423a3f342fb5ca3077dbe
Christos Karavasileiadis <ckaravasileiadis@ganintegrity.com>
2019-09-27 16:13:25 +0200

    chore: done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Message {
+  text: string;
+  createdBy: User;
+}
+
+const someUsers: User[] = [
+  {
+    name: "Christos",
+    email: "christos@domain.com"
+  },
+  {
+    name: "A friend",
+    email: "friend@domain.com"
+  }
+];
+
+const someMessages: Message[] = [
+  {
+    text: "Hi. How are you?",
+    createdBy: someUsers[0]
+  },
+  {
+    text: "I am fine thanks. How are you?",
+    createdBy: someUsers[1]
+  }
+];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'templateLocalVariable48146212';
+  currentUser: User = someUsers[0];
+  users: User[] = someUsers;
+  messages: Message[] = someMessages;
 }
